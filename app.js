44c6a5d08429f2ad20da62e48e3366c762b59015
cyZ52 <1349706931@qq.com>
2024-04-09 22:16:26 +0800

    上传图片

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/') // 设置文件上传目录
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)) // 设置文件名
+  }
+});
+
+const upload = multer({ storage: storage });
 
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
@@ -17,7 +29,10 @@ var productRouter = require('./routes/product');
 var orderRouter = require('./routes/order');
 var discussRouter = require('./routes/discuss');
 
+
 var app = express();
+
+app.use('/uploads', express.static('uploads'));
 
 const {DBHOST, DBPORT, DBNAME} = require('./config/config');
 
@@ -72,6 +87,34 @@ app.use('/foster', fosterRouter);
 app.use('/product', productRouter);
 app.use('/order', orderRouter);
 app.use('/discuss',discussRouter);
+
+app.post('/uploadImg', upload.single('file'), function (req, res) {
+  if (req.file) {
+      console.log('文件已上传:', req.file);
+
+      // 使用上传文件的临时路径
+      const filePath = req.file.path;
+      const formattedPath = filePath.replace(/\\/g, '/');
+      console.log('上传文件的临时路径:', formattedPath);
+
+      // 获取上传文件的原始文件名和扩展名
+      const originalname = req.file.originalname;
+      const ext = path.extname(originalname);
+
+      // 构建图片的URL，包含上传文件的后缀名
+      const imageUrl = `${req.protocol}://${req.get('host')}/${formattedPath}`;
+      console.log('图片URL:', imageUrl);
+
+      // 在这里处理上传的文件，如保存到服务器或进行其他操作
+      res.json({
+          msg: '文件上传成功',
+          url: imageUrl
+      });
+  } else {
+      res.status(400).json({ error: '未找到上传的文件' });
+  }
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/acccount.js
+++ routes/acccount.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// const multer = require('multer');
+// const upload = multer({ dest: 'uploads/' });
 
 const md5 = require('md5');
 
@@ -8,7 +12,7 @@ const UserModel = require('../models/user');
 
 
 // 获取所有账号信息
-router.get('/getAccounts', function (req ,res, next) {
+router.get('/getAccounts', function (req, res, next) {
     UserModel.find({}).then((data) => {
         res.json({
             data: data
@@ -20,30 +24,72 @@ router.get('/getAccounts', function (req ,res, next) {
 router.post('/changeUserInfo', function (req, res, next) {
     console.log('需要修改的信息：', req.body);
     UserModel.updateOne({ username: req.body.username }, { $set: { ...req.body } }).then(() => {
-      res.json({
-        msg: '修改成功'
-      })
-    })
-  })
-
-// 重置账号密码
-router.post('/resetPassword', function(req, res, next) {
-    const defaultPassword = '123456';
-    UserModel.updateOne({ username: req.body.username}, { $set: {password: md5(defaultPassword)}}).then(() => {
         res.json({
-            msg: '修改密码成功'
+            msg: '修改成功'
         })
     })
-  })
+})
 
-// 修改账号密码
-router.post('/changePassword', function(req, res, next) {
+// 修改头像
+router.post('/changeAvater', function (req, res, next) {
+    console.log(req.body)
+    UserModel.updateOne({ username: req.body.username }, { $set: { avater: req.body.avater } }).then(() => {
+        res.json({
+            msg: '修改成功'
+        })
+    })
+})
+
+// 重置账号密码
+router.post('/resetPassword', function (req, res, next) {
     const defaultPassword = '123456';
-    UserModel.updateOne({ username: req.body.username}, { $set: {password: md5(req.body.password)}}).then(() => {
+    UserModel.updateOne({ username: req.body.username }, { $set: { password: md5(defaultPassword) } }).then(() => {
         res.json({
             msg: '修改密码成功'
         })
     })
 })
+
+// 修改账号密码
+router.post('/changePassword', function (req, res, next) {
+    const defaultPassword = '123456';
+    UserModel.updateOne({ username: req.body.username }, { $set: { password: md5(req.body.password) } }).then(() => {
+        res.json({
+            msg: '修改密码成功'
+        })
+    })
+})
+
+// text
+// router.post('/text', upload.single('file'), function (req, res) {
+//     if (req.file) {
+//         console.log('文件已上传:', req.file);
+
+//         // 使用上传文件的临时路径
+//         const filePath = req.file.path;
+//         const formattedPath = filePath.replace(/\\/g, '/');
+//         console.log('上传文件的临时路径:', formattedPath);
+
+//         // 获取上传文件的原始文件名和扩展名
+//         const originalname = req.file.originalname;
+//         const ext = path.extname(originalname);
+
+//         const src = `${req.protocol}://${req.get('host')}/${formattedPath}`;
+
+//         // 构建图片的URL，包含上传文件的后缀名
+//         const imageUrl = `${req.protocol}://${req.get('host')}/${formattedPath}${ext}`;
+//         console.log('图片URL:', imageUrl);
+
+//         // 在这里处理上传的文件，如保存到服务器或进行其他操作
+//         res.json({
+//             msg: '文件上传成功',
+//             src: src,
+//             url: imageUrl
+//         });
+//     } else {
+//         res.status(400).json({ error: '未找到上传的文件' });
+//     }
+// });
+
 
 module.exports = router;
